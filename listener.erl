d6b0ff66150bff9c8f2585d33017b73e7b1ed75a
Mateusz Zawisza <mateusz@applicake.com>
2012-12-07 14:46:27 +0100

    get rid of the warnings

--- listener.erl
+++ listener.erl
@@ -1,27 +1,24 @@
 -module(listener).
 -export([server/1]).
--export([listen/1]).
--export([proxy_request/1]).
 
 server(Port) ->
   {ok,Socket} = gen_udp:open(Port,[binary]),
+  inets:start(),
   listen(Socket).
 
 listen(Socket) ->
   receive
-    {udp,Socket,Host,Port,Bin} = Message ->
-      {udp,MPort,MHost,MPort2,Data} = Message,
+    {udp,Socket,_Host,_Port,_Bin} = Message ->
+      {_udp,_MPort,_MHost,_MPort2,Data} = Message,
       proxy_request(Data),
       listen(Socket)
   end.
 
 proxy_request(Data) ->
-  inets:start(),
   {Code, Message} = httpc:request(post, { "http://localhost:9393/", [], [], Data}, [], []),
   handle_response({Code, Message}).
 
-handle_response({ok, Data}) ->
-  %% io:format("OK Response: ~p~n",[Data]);
+handle_response({ok, _}) ->
   io:format("OK");
 handle_response({error, Data}) ->
   io:format("Error Response: ~p~n",[Data]).
